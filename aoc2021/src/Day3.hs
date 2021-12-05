b3c655c2e228abe3d9e2eec7b604c3d2d7c70c97
Oleg Nykolyn <juravel2@gmail.com>
2021-12-05 15:11:20 +0200

    Refactor

--- aoc2021/src/Day3.hs
+++ aoc2021/src/Day3.hs
@@ -11,45 +11,56 @@ import           Import        hiding (Down, to, (%~))
 import           Prelude       (print)
 import qualified RIO.Text      as T
 
+-- Issues:
+-- - BitCount is hardcoded - 11
+-- - Manual recursion for `findNum`
+-- - Stat is reused for {0,1} and for real statistics - concise but confusing
+
 main :: IO ()
 main = do
     lines_ <- readLinesT "data/3.txt"
     let parsedLines = parseList lines_
-        cutoffCount = (length lines_) `div` 2
-    print $ parsedLines
+        cutoffCount = length lines_ `div` 2
+    print parsedLines
     print $ foldl1 sumStats parsedLines
     print $ getNumbers (foldl1 sumStats parsedLines) cutoffCount
     print $ getNumbers2 parsedLines
 
+-- Observations:
+-- (Int, BitsCount) <-> Stat <-> "0b00101010" are isomorphic
+
 getNumbers2 :: [Stat] ->(Int, Int)
-getNumbers2 stats = (oxy, co2)
+getNumbers2 stats_ = (oxy, co2)
     where
         oxy = constructBinary $ findNum (\value cutoff -> case value `compare` cutoff of
             LT -> 0
             EQ -> 1
             GT -> 1
             )
-            stats 11
+            stats_ 11
         co2 = constructBinary $ findNum (\value cutoff -> case value `compare` cutoff of
             LT -> 1
             EQ -> 0
             GT -> 0
             )
-            stats 11
+            stats_ 11
         findNum :: (Int -> Int -> Int) -> [Stat] -> Int -> Stat
+        -- findNum can fail in two ways:
+        -- running out of bits
+        -- running out of [Stat]
         findNum f stats ii =
             let st = foldl1 sumStats stats
-                cutoff = (lengthOf folded stats) `div` 2 + (lengthOf folded stats) `mod` 2
+                cutoff = lengthOf folded stats `div` 2 + lengthOf folded stats `mod` 2
             in
             case lengthOf folded stats of
             1 -> stats ^. folded
             0 -> error "All filtered out!"
-            _ -> traceShow ("BIT: " <> show ii) $ traceShow stats $ findNum f (stats ^.. folded . filtered
+            _ -> findNum f (stats ^.. folded . filtered
                 (\v -> M.lookup ii v == ((\sv -> f sv cutoff) <$> M.lookup ii st)))
                 (ii-1)
         constructBinary :: Stat -> Int
         constructBinary s =
-            M.foldrWithKey (\index val bin -> if val == 1 then setBit bin index else bin) zeroBits s
+            M.foldrWithKey (\idx val bin -> if val == 1 then setBit bin idx else bin) zeroBits s
 
 
 getNumbers :: Stat -> Int -> (Int, Int)
@@ -58,7 +69,7 @@ getNumbers st cutoff = (gamma, eplison)
         gamma = constructBinary (>cutoff)
         eplison = constructBinary (<=cutoff)
         constructBinary f =
-            M.foldrWithKey (\index val bin -> if f val then setBit bin index else bin) zeroBits st
+            M.foldrWithKey (\idx val bin -> if f val then setBit bin idx else bin) zeroBits st
 
 parseList :: [Text] -> [Stat]
 parseList = fmap parseStat
